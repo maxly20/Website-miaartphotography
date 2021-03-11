b4b3a1631e2c01ef8cc13cfbbd7e01d3456e4639
maxly20 <maxly2011@gmail.com>
2021-03-11 19:07:45 +1100

    finish homepage

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName('mySlides');
-  var dots = document.getElementsByClassName('dot');
+
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -24,11 +24,8 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
+
   slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
 }
 
 // slideshow
